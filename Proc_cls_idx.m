7408809dfa6ca7d339c6e2068896053e32293d9c
zhihaofan <59326288+zhihaofan@users.noreply.github.com>
2022-07-07 22:16:45 +0800

    Update Proc_cls_idx.m

--- Proc_cls_idx.m
+++ Proc_cls_idx.m
@@ -1,7 +1,6 @@
-function  [s_idx, seg]    =  Proc_cls_idx( cls_idx )    %类的序号  属于的类别 cls_idx 每个块所属于的类别
-[idx  s_idx]    =  sort(cls_idx);          %idx 存放类序号排序结果第一类，第二类...，
-                                           %s_idx 排序结果索引块的位置，属于第一类得块，属于第二类的块，
-idx2   =  idx(1:end-1) - idx(2:end);   %错位相减
-seq    =  find(idx2);    %63*1 错位相减后有263个类序号差非零，返回对应块的索引位置
+function  [s_idx, seg]    =  Proc_cls_idx( cls_idx )   
+[idx  s_idx]    =  sort(cls_idx);         
+idx2   =  idx(1:end-1) - idx(2:end);  
+seq    =  find(idx2);   
 
 seg    =  [0; seq; length(cls_idx)];   
