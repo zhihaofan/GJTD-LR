c7049b0a2f25c4db65a7d446af5583dba7d10c18
zhihaofan <59326288+zhihaofan@users.noreply.github.com>
2022-07-07 22:11:16 +0800

    Update FDDL.m

--- FDDL.m
+++ FDDL.m
@@ -1,5 +1,4 @@
 function [Dict,Drls,coef] = FDDL(TrainDat,TrainLabel,opts)
-%%   进度：over
 %%%%%%%%%%%%%%%%%%
 % normalize energy
 %%%%%%%%%%%%%%%%%%
@@ -13,18 +12,18 @@ end
 %%%%%%%%%%%%%%%%%%
 %initialize dict  
 %%%%%%%%%%%%%%%%%% 
-Dict_ini  =  [];   %初始化的字典为空，用来存放初始化的字典
-Dlabel_ini = [];   %对应的标签
+Dict_ini  =  [];  
+Dlabel_ini = []; 
 
 for ci = 1:opts.nClass
-    cdat          =    TrainDat(:,TrainLabel==ci,:);    %第一类：300*7 即：7个300维的样本
-%     dict          =    FDDL_INID(cdat,size(cdat,2),opts.wayInit);  %对第一类（7个样本）而言，用pca初始化第一类的子字典（300*7）
+    cdat          =    TrainDat(:,TrainLabel==ci,:);   
+%     dict          =    FDDL_INID(cdat,size(cdat,2),opts.wayInit);  
     temp=zeros(m1,size(cdat,2),k1);
     for i=1:k1
-        dict=FDDL_INID(cdat,size(cdat,2),opts.wayInit);         % 100：每一类原子数
+        dict=FDDL_INID(cdat,size(cdat,2),opts.wayInit);         
         temp(:,:,i)=dict;
     end
-    Dict_ini      =    [Dict_ini temp];      %1：7列为第一类的初始化字典，8：14列为第二类的初始化子字典
+    Dict_ini      =    [Dict_ini temp];     
     Dlabel_ini    =    [Dlabel_ini repmat(ci,[1 size(temp,2)])];
 end
 
@@ -32,18 +31,16 @@ end
 %    dict_path = ['E:\Matlab2016a\Aworkplace\2020classify_SR\Dictionary\D_ini' '.mat' ];
 %    save(dict_path, 'Dict_ini');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 一类一类初始化编码系数
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ini_par.tau         =     opts.lambda1;
 ini_par.lambda      =     opts.lambda2;
-ini_ipts.D          =     Dict_ini;            %初始化的大字典
+ini_ipts.D          =     Dict_ini;            
  coef = zeros(size(Dict_ini,2),size(TrainDat,2),k1);
-if size(Dict_ini,1)>size(Dict_ini,2)           %d = eigs(A,k) %返回k个最大特征值
+if size(Dict_ini,1)>size(Dict_ini,2)          
       ini_par.c        =    1.05*eigs(Dict_ini(:,:,1)'*Dict_ini(:,:,1),1);
 else
       ini_par.c        =    1.05*eigs(Dict_ini(:,:,1)*Dict_ini(:,:,1)',1);
 end
-for ci =  1:opts.nClass     %一类一类的初始化编码系数
+for ci =  1:opts.nClass    
     fprintf(['Initializing Coef:  Class ' num2str(ci) '\n']);
     ini_ipts.X      =    TrainDat(:,TrainLabel==ci,:);
     [ini_opts]      =    FDDL_INIC (ini_ipts,ini_par);
@@ -54,9 +51,9 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Main loop of Dictionary Learning
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
- Fish_par.dls        =     Dlabel_ini;   %字典标签
- Fish_ipts.D         =     Dict_ini;     %初始化字典
- Fish_ipts.trls      =     TrainLabel;    %样本类别标签
+ Fish_par.dls        =     Dlabel_ini;  
+ Fish_ipts.D         =     Dict_ini;   
+ Fish_ipts.trls      =     TrainLabel;   
  Fish_par.tau        =     opts.lambda1;
  Fish_par.lambda2    =     opts.lambda2;
  Fish_nit            =     1;
@@ -65,7 +62,6 @@ end
  CoefM=zeros(size(Fish_ipts.D,2),1,size(Fish_ipts.D,3));
  mu=0.0001;
  mu_1=0.5;
- % 张量逆，傅里叶变换求逆，在逆变换
  D=Fish_ipts.D;
  A=coef;
  
