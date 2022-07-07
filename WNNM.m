d3fae66aff4d9912f6db10fc472e2776eb3cfbcf
zhihaofan <59326288+zhihaofan@users.noreply.github.com>
2022-07-07 22:17:20 +0800

    Update WNNM.m

--- WNNM.m
+++ WNNM.m
@@ -2,8 +2,7 @@
 function  [X] =  WNNM( Y, C, NSig)
 [U,SigmaY,V] = svd(full(Y),'econ');    
 PatNum       = size(Y,2);
-%     TempC  = C*sqrt(PatNum)*2*NSig^2;
 TempC  = C*NSig^2;
-[SigmaX,svp] = ClosedWNNM(SigmaY,TempC,eps); % epsMATLAB默认的最小浮点数精度.    sigmaX是X的奇异值，分别为d1,d2……，svp是矩阵中大于0的个数
-X =  U(:,1:svp)*diag(SigmaX)*V(:,1:svp)';     % 用奇异值恢复X
+[SigmaX,svp] = ClosedWNNM(SigmaY,TempC,eps); 
+X =  U(:,1:svp)*diag(SigmaX)*V(:,1:svp)';  
 return;
