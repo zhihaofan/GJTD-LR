0226f0368e64a3b77334cddeafe6d79380cbf1d8
zhihaofan <59326288+zhihaofan@users.noreply.github.com>
2022-07-07 22:08:47 +0800

    Update ClosedWNNM.m

--- ClosedWNNM.m
+++ ClosedWNNM.m
@@ -1,7 +1,7 @@
-function [SigmaX,svp]=ClosedWNNM(SigmaY,C,oureps)           % SigmaY:svd特征值矩阵
-temp=(SigmaY-oureps).^2-4*(C-oureps*SigmaY);  % .*:对应元素相乘，值为c2
-ind=find (temp>0);   % 找到矩阵>0的位置
+function [SigmaX,svp]=ClosedWNNM(SigmaY,C,oureps)           
+temp=(SigmaY-oureps).^2-4*(C-oureps*SigmaY);  
+ind=find (temp>0);   
 svp=length(ind);
-SigmaX=max(SigmaY(ind)-oureps+sqrt(temp(ind)),0)/2;             % 软阈值
+SigmaX=max(SigmaY(ind)-oureps+sqrt(temp(ind)),0)/2;            
 
 end
